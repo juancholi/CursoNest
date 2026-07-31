14df65b518ffa353030f4ade93c411000e95dbdb
Juan Olivares <juan.olivares74@gmail.com>
2026-07-31 11:17:28 -0400

    seccion 8 - insertar multiples registros v2

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -20,31 +20,30 @@ export class SeedService {
 
   async executeSeed(limit: number) {
 
-    // const createPokemonDto: CreatePokemonDto[] = [];
+    const pokemonToInsert: CreatePokemonDto[] = []; // insertar multiples registros v2
     await this.pokemonModel.deleteMany({}); // delete * from pokemons
 
     const{data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
 
-    const insertPromisesArray = [];
+    // const insertPromisesArray = []; // insertar multiples registros v1
 
     data.results.forEach(async ({name, url}) => {
 
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({segments, no})
-      // createPokemonDto.push({name,no})
+      // console.log({segments, no})
+      pokemonToInsert.push({name,no}) // insertar multiples registros v2
 
       // this.pokemonService.create({name,no})
       // await this.pokemonModel.create({name,no}) // no es practico para grandes volumenes de datos
 
-      insertPromisesArray.push(
-        this.pokemonModel.create({name,no})
-      );
+      // insertPromisesArray.push(this.pokemonModel.create({name,no})); // insertar multiples registros v1
 
     });
 
-    await Promise.all( insertPromisesArray );
+    // await Promise.all( insertPromisesArray ); // insertar multiples registros v1
+    await this.pokemonModel.insertMany(pokemonToInsert); // insertar multiples registros v2 (es el simil de 'insert into pokemon (name, no)')
 
     // this.pokemonService.create()
     return `Seed executed`;
